2375cddb9b1bfc0d9149caa2054cb2c88ef92d53
Fardan Nozami <fardan.nozami@gmail.com>
2021-03-22 21:51:09 +0700

    menambahkan file index.js

---
+++ index.js
@@ -0,0 +1,54 @@
+// konsep this pada arrow function
+
+// constructor function
+// const Mahasiswa = function () {
+//   this.nama = "Fardan Nozami";
+//   this.umur = 28;
+//   this.sayHello = function () {
+//     console.log(`Halo nama saya ${this.nama}, umur saya ${this.umur} tahun`);
+//   };
+// };
+
+// const fardan = new Mahasiswa();
+// fardan.sayHello();
+
+// Arrow function
+// const Mahasiswa = function () {
+//   this.nama = "Fardan Nozami";
+//   this.umur = 28;
+//   this.sayHello = () => {
+//     console.log(`Halo nama saya ${this.nama}, umur saya ${this.umur} tahun`);
+//   };
+// };
+
+// const fardan = new Mahasiswa();
+// fardan.sayHello();
+
+// Object literal
+
+// const mhs1 = {
+//   nama: "Fardan Nozami",
+//   umur: 28,
+//   sayHello: () => {
+//     console.log(`Halo nama saya ${this.nama}, umur saya ${this.umur} tahun`);
+//   },
+// };
+// mhs1.sayHello();
+
+// const Mahasiswa = function () {
+//   this.nama = "Fardan Nozami";
+//   this.umur = 28;
+//   this.sayHello = function () {
+//     console.log(`Halo nama saya ${this.nama}, umur saya ${this.umur} tahun`);
+//   };
+
+//   setInterval(() => {
+//     console.log(this.umur++);
+//   }, 500);
+// };
+
+// const fardan = new Mahasiswa();
+// fardan.sayHello();
+
+
+
